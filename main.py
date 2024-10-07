04df63f26554ce82aa8c4720371904c59bea8ced
Agustina Belén Fernández <agustinabelenfernandez05@gmail.com>
2024-10-07 10:49:10 -0300

    ADD: Definiendo cargar inventario

---
+++ main.py
@@ -0,0 +1,27 @@
+def mostrar_menu_principal():
+    """ 
+    Muestra el menú de opciones
+    """
+    print(""" 
+    1. Cargar producto/s
+    2. Buscar producto
+    3. Ordenar inventario
+    4. Mostrar producto más caro y más barato
+    5. Mostrar productos con precio mayor a 15000
+    6. Salir
+    """)
+
+def cargar_inventario()
+
+
+
+
+
+
+
+
+def menu_principal():
+    print("Bienvenido!...Elija una de las opciones")
+    mostrar_menu_principal()
+
+
